8fa28269a9541a5868510ced5d385c18704022d4
yashrasniya <67114982+yashrasniya@users.noreply.github.com>
2021-12-17 13:13:32 +0530

    Update c.c

--- c.c
+++ c.c
@@ -7,30 +7,36 @@ struct node *leftchid;
 struct node *rightchild;
 
 };
-struct node* root=(struct *node)malloc(sizeof(struct node));
-struct node* prv=root;
 
 
-void insert(int data){
-struct node *temp=(struct node)malloc(sizeof(struct node));
+
+struct node* insert(int data,struct node *prv){
+struct node *temp=(struct node *)malloc(sizeof(struct node));
 temp->data=data;
 temp->leftchid=NULL;
 temp->leftchid=NULL;
-if(prv->leftchid!=NULL){
+if(prv->leftchid==NULL){
   prv->leftchid=temp ;
 }
-else if(prv->rightchild!=NULL){
+else if(prv->rightchild==NULL){
       prv->rightchild=temp;
 }else{
 printf("node is full");
 }
 prv=temp;
+return prv;
 }
+
 int main(){
+
+struct node* root =(struct node *)malloc(sizeof(struct node));
 root->data=22;
 
-insert(3);
-insert(24);
-insert(34);
-}
+struct node* prv = root;
+prv=insert(1,prv);
+prv=insert(3,root);
+prv=insert(7,root);
+
+
+
 }
